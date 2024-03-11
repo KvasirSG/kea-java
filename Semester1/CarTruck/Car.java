91ee3ac6db8729733a424a4b274acb5965ed5d27
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-03-11 15:15:53 +0000

    adding car to CarTruck Assignment

---
+++ Semester1/CarTruck/Car.java
@@ -0,0 +1,28 @@
+package Semester1;
+
+public class Car {
+    private int year;
+    private int price;
+    private String colour;
+    private int gears = 5;
+
+    public Car(int year, int price, String colour){
+        this.year=year;
+        this.price=price;
+        this.colour=colour;
+    }
+    public int getGears() {
+        return gears;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
