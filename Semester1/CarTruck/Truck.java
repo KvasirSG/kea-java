e3d395d50db4c43e34f215761ea4c98ad5b0c031
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-03-11 18:53:59 +0000

    added the truck class

---
+++ Semester1/CarTruck/Truck.java
@@ -0,0 +1,29 @@
+package Semester1.CarTruck;
+
+public class Truck extends  Car{
+    private int bruttoLoad;
+    private boolean hanger;
+
+
+    public Truck(int year, int price, String colour, int bruttoLoad, boolean hanger) {
+        super(year, price, colour);
+        this.bruttoLoad = bruttoLoad;
+        this.hanger = hanger;
+    }
+
+    public int getBruttoLoad() {
+        return bruttoLoad;
+    }
+
+    public void setBruttoLoad(int bruttoLoad) {
+        this.bruttoLoad = bruttoLoad;
+    }
+
+    public boolean isHanger() {
+        return hanger;
+    }
+
+    public void setHanger(boolean hanger) {
+        this.hanger = hanger;
+    }
+}
