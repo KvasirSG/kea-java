5403756ee76179e63383e7448697f7f8b35ce31c
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-03-12 08:03:40 +0000

    making updates to the broker class

--- Semester1/CarTruck/Broker.java
+++ Semester1/CarTruck/Broker.java
@@ -2,6 +2,14 @@ package Semester1.CarTruck;
 
 public class Broker {
     public static void main(String[] args) {
-        
+        Car[] cars = new Car[49];
+        cars[0] = new Car(1999,40000,"Yellow");
+        cars[1] = new Truck(2004,60000,"Black",700,true);
+        printCars(cars);
+    }
+    public static void printCars(Car[] cars){
+        for (Car car:cars){
+            System.out.println(car);
+        }
     }
 }
